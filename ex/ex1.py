700728d08ccd77b3f49f769f18a80708312efa8d
eastflag <dklee98@>
2020-12-21 17:57:17 +0900

    .

--- ex/ex1.py
+++ ex/ex1.py
@@ -1,3 +1,5 @@
+# map의 역할, 문자열 리스트를 숫자형 리스트로 변환,
+# array destructing 역할
 a = '123456789'
 b = list(a)
 c = map(int, b)
---
+++ ex/ex2.py
@@ -0,0 +1,14 @@
+# pass
+for i in range(10):
+    if i % 2 == 0:
+        # pass
+        print(i)
+    else:
+        print(i)
+print("Done")
+# in or not in 멤버연산자: 배열에 해당 요소가 있으면 true 없으면 false
+a = 'aaabcda'
+if 'a' in a[3:]:
+    print('true')
+else:
+    print('false')
